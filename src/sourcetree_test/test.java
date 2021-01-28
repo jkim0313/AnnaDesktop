4914457aa9d560156a3a641bc0b1291df4b94161
jkim0313 <jkim940313@gmail.com>
2021-01-28 19:49:13 +0900

    소스트리 연동 인증

--- src/sourcetree_test/test.java
+++ src/sourcetree_test/test.java
@@ -3,7 +3,7 @@ package sourcetree_test;
 public class test {
 
 	public static void main(String[] args) {
-		String abc = "abc";
+		String abc = "소스트리 깃허브 연동 테스트";
 		
 		System.out.println("abc");
 
